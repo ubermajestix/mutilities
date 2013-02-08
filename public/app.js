8040b740813354350eaa7d771ffbecdc378d789b
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2013-02-08 10:16:45 -0800

    Big refactor

--- public/app.js
+++ public/app.js
@@ -1,69 +1,85 @@
- Mute = function(){
-  var self = this;
-  this.me = gapi.hangout.getLocalParticipant();
-  $('button.mute').click(function(){ 
-    self.mute();
-  });
-  this.push_to_talk();
+var Hangout = {
+  me: gapi.hangout.getLocalParticipant(),
+  participants: function(){ return gapi.hangout.getParticipants();},
+  mute: function(participant){
+    if(_.isEmpty(arguments)){
+      participant = Hangout.me;
+    }
+    gapi.hangout.av.muteParticipantMicrophone(participant.id);
+  },
+  talk: function(){
+    gapi.hangout.av.setMicrophoneMute(false);
+  }
 };
 
-Mute.prototype = {
+var Mute = {
+  init: function(){
+    Mute.push_to_talk_state = false;
+    Mute.bind_mute_everyone();
+    Mute.bind_push_to_talk();
+  },
+
   icon: function(icon){
     return "<i class='icon-" + icon + "'></i>";
   },
-  participants: function(){ return gapi.hangout.getParticipants();},
-  mute: function(){
-    _.each(this.participants, function(participant){
-      if(participant != this.me){
-        gapi.hangout.av.muteParticipantMicrophone(participant.id);
+
+  bind_mute_everyone: function(){
+    $('button.mute').click(function(){ 
+      self.mute_everyone();
+    });
+  },
+
+  mute_everyone: function(){
+    _.each(Mute.participants(), function(participant){
+      if(participant != Mute.me){
+        Mute.mute(participant);
       }
     });
   },
+
+  bind_push_to_talk: function(){
+    bind_settings_dropdown();
+    $('button.push_to_talk').mousedown(function(){
+      push_to_talk();
+      $(this).toggleClass('btn-danger');
+    });
+    $('button.push_to_talk').mouseup(function(){
+      push_to_mute();
+      $(this).toggleClass('btn-danger');
+    });
+    $("body").keydown(function(event) {
+      if ( event.which == 77 ) {
+        push_to_talk();
+        $(this).toggleClass('btn-danger');
+      }
+    });
+    $("body").keyup(function(event) {
+      if ( event.which == 77 ) {
+        push_to_mute();
+        $(this).toggleClass('btn-danger');
+      }
+    });
+  },
+
   push_to_talk: function(){
-    var self = this;
-    var push_to_talk = false;
+    push_to_talk_state ? Hangout.talk() : Hangout.mute();
+  },
+
+  push_to_mute: function(){
+    push_to_talk_state ? Hangout.mute() : Hangout.talk();
+  },
+
+  bind_settings_dropdown: function(){
     $('a.push_to_mute').hide();
     $('a.push_to_talk').click(function(){
-      console.log('talk');
-      push_to_talk = true;
+      push_to_talk_state = true;
       $('a.push_to_talk, a.push_to_mute').toggle();
       $('button.push_to_talk').html(self.icon('bullhorn') + 'push to talk').addClass('btn-danger');
     });
     $('a.push_to_mute').click(function(){
-      console.log('mute');
-      push_to_talk = false;
+      push_to_talk_state = false;
       $('a.push_to_talk, a.push_to_mute').toggle();
       $('button.push_to_talk').html(self.icon('ban-circle') + 'push to mute');
-    });
-    $('button.push_to_talk').mousedown(function(){
-      if(push_to_talk){
-        $(this).removeClass('btn-danger');
-      }
-      else{
-        $(this).addClass('btn-danger');
-      }
-      gapi.hangout.av.setMicrophoneMute(!push_to_talk);
-    });
-    $('button.push_to_talk').mouseup(function(){
-      if(push_to_talk){
-        $(this).addClass('btn-danger');
-      }
-      else{
-        $(this).removeClass('btn-danger');
-      }
-      gapi.hangout.av.setMicrophoneMute(push_to_talk);
-    });
-    $("body").keydown(function(event) {
-      console.log(event.which);
-      if ( event.which == 77 ) {
-        gapi.hangout.av.setMicrophoneMute(!push_to_talk);
-      }
-    });
-    $("body").keyup(function(event) {
-      console.log(event.which);
-      if ( event.which == 77 ) {
-        gapi.hangout.av.setMicrophoneMute(push_to_talk);
-      }
     });
   }
 };
@@ -71,8 +87,7 @@ Mute.prototype = {
 function init() {
   var apiReady = function(eventObj) {
     if (eventObj.isApiReady) {
-      var mute = new Mute();
-      console.log(mute);
+      Mute.init();
     }
   };
   gapi.hangout.onApiReady.add(apiReady);
