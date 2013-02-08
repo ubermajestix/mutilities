dcce3551a3d30e2ac75b730e51d2a6e2b32c3277
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2013-02-07 16:06:51 -0800

    Refactor js and ui

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
-function init() {
-  console.log('Init app.');
+function Mute(){
+  var self = this;
+  this.me = gapi.hangout.getLocalParticipant();
+  this.push_to_talk = true;
+  $('button.mute').click(function(){ 
+    self.mute();
+  });
+  this.push_to_talk();
+}
 
+Mute.prototype = {
+  participants: function(){ return gapi.hangout.getParticipants();},
+  mute: function(){
+    _.each(this.participants, function(participant){
+      if(participant != this.me){
+        gapi.hangout.av.muteParticipantMicrophone(participant.id);
+      }
+    });
+  },
+  push_to_talk: function(){
+    var self = this;
+    $('a.push_to_talk').click(function(){
+      self.push_to_talk = true;
+      $('button.push_to_talk').text('push to talk');
+    });
+    $('a.push_to_mute').click(function(){
+      self.push_to_talk = false;
+      $('button.push_to_talk').text('push to talk');
+    });
+    $('button.push_to_talk').mousedown(function(){
+      gapi.hangout.av.setMicrophoneMute(!push_to_talk);
+    });
+    $('button.push_to_talk').mouseup(function(){
+      gapi.hangout.av.setMicrophoneMute(push_to_talk);
+    });
+  }
+};
+
+function init() {
   var apiReady = function(eventObj) {
     if (eventObj.isApiReady) {
-      console.log('API is ready');
-      console.log(gapi.hangout);
-      console.log(gapi.hangout.getParticipants());
-      var me = gapi.hangout.getLocalParticipant();
-      $('button.mute').click(function(){
-        var participants = gapi.hangout.getParticipants();
-        _.each(participants, function(participant){
-          console.log(participant);
-          gapi.hangout.av.muteParticipantMicrophone(participant.id);
-        });
-      });
-      var push_to_talk = true;
-      $('input[name="push_to_talk"]').change(function(){
-         if($(this).val()=='mute'){
-           push_to_talk = false;
-           $('button.push_to_talk').text('push to mute');
-         }
-         else{
-           push_to_talk = true;
-           $('button.push_to_talk').text('push to talk');
-         }
-      });
-      $('button.push_to_talk').mousedown(function(){
-        me.setMicrophoneMute(!push_to_talk);
-      });
-      $('button.push_to_talk').mouseup(function(){
-        me.setMicrophoneMute(push_to_talk);
-      });
+      var mute = new Mute();
     }
   };
-
-  // This application is pretty simple, but use this special api ready state
-  // event if you would like to any more complex app setup.
   gapi.hangout.onApiReady.add(apiReady);
 }
-console.log('its alive!');
 gadgets.util.registerOnLoadHandler(init);
