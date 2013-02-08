b822466301c2556d43f5ebd7d43de0ba7f0e72df
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2013-02-08 10:36:58 -0800

    Change to local dev
    Add local.xml

--- public/app.js
+++ public/app.js
@@ -14,6 +14,7 @@ var Hangout = {
 
 var Mute = {
   init: function(){
+    console.log('init');
     Mute.push_to_talk_state = false;
     Mute.bind_mute_everyone();
     Mute.bind_push_to_talk();
@@ -38,46 +39,47 @@ var Mute = {
   },
 
   bind_push_to_talk: function(){
-    bind_settings_dropdown();
+    console.log('bind_push_to_talk');
+    Mute.bind_settings_dropdown();
     $('button.push_to_talk').mousedown(function(){
-      push_to_talk();
+      Mute.push_to_talk();
       $(this).toggleClass('btn-danger');
     });
     $('button.push_to_talk').mouseup(function(){
-      push_to_mute();
+      Mute.push_to_mute();
       $(this).toggleClass('btn-danger');
     });
     $("body").keydown(function(event) {
       if ( event.which == 77 ) {
-        push_to_talk();
+        Mute.push_to_talk();
         $(this).toggleClass('btn-danger');
       }
     });
     $("body").keyup(function(event) {
       if ( event.which == 77 ) {
-        push_to_mute();
+        Mute.push_to_mute();
         $(this).toggleClass('btn-danger');
       }
     });
   },
 
   push_to_talk: function(){
-    push_to_talk_state ? Hangout.talk() : Hangout.mute();
+    Mute.push_to_talk_state ? Hangout.talk() : Hangout.mute();
   },
 
   push_to_mute: function(){
-    push_to_talk_state ? Hangout.mute() : Hangout.talk();
+    Mute.push_to_talk_state ? Hangout.mute() : Hangout.talk();
   },
 
   bind_settings_dropdown: function(){
     $('a.push_to_mute').hide();
     $('a.push_to_talk').click(function(){
-      push_to_talk_state = true;
+      Mute.push_to_talk_state = true;
       $('a.push_to_talk, a.push_to_mute').toggle();
       $('button.push_to_talk').html(self.icon('bullhorn') + 'push to talk').addClass('btn-danger');
     });
     $('a.push_to_mute').click(function(){
-      push_to_talk_state = false;
+      Mute.push_to_talk_state = false;
       $('a.push_to_talk, a.push_to_mute').toggle();
       $('button.push_to_talk').html(self.icon('ban-circle') + 'push to mute');
     });
@@ -87,6 +89,7 @@ var Mute = {
 function init() {
   var apiReady = function(eventObj) {
     if (eventObj.isApiReady) {
+      console.log('ready');
       Mute.init();
     }
   };
