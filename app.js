7b60f1976f60d60067bca453281e2c5df5729dbb
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2013-02-07 16:57:24 -0800

    Ready for primetime

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-function Mute(){
+ Mute = function(){
   var self = this;
   this.me = gapi.hangout.getLocalParticipant();
-  this.push_to_talk = true;
   $('button.mute').click(function(){ 
     self.mute();
   });
   this.push_to_talk();
-}
+};
 
 Mute.prototype = {
+  icon: function(icon){
+    return "<i class='icon-" + icon + "'></i>";
+  },
   participants: function(){ return gapi.hangout.getParticipants();},
   mute: function(){
     _.each(this.participants, function(participant){
@@ -19,19 +21,49 @@ Mute.prototype = {
   },
   push_to_talk: function(){
     var self = this;
+    var push_to_talk = false;
+    $('a.push_to_mute').hide();
     $('a.push_to_talk').click(function(){
-      self.push_to_talk = true;
-      $('button.push_to_talk').text('push to talk');
+      console.log('talk');
+      push_to_talk = true;
+      $('a.push_to_talk, a.push_to_mute').toggle();
+      $('button.push_to_talk').html(self.icon('bullhorn') + 'push to talk').addClass('btn-danger');
     });
     $('a.push_to_mute').click(function(){
-      self.push_to_talk = false;
-      $('button.push_to_talk').text('push to talk');
+      console.log('mute');
+      push_to_talk = false;
+      $('a.push_to_talk, a.push_to_mute').toggle();
+      $('button.push_to_talk').html(self.icon('ban-circle') + 'push to mute');
     });
     $('button.push_to_talk').mousedown(function(){
+      if(push_to_talk){
+        $(this).removeClass('btn-danger')
+      }
+      else{
+        $(this).addClass('btn-danger')
+      }
       gapi.hangout.av.setMicrophoneMute(!push_to_talk);
     });
     $('button.push_to_talk').mouseup(function(){
+      if(push_to_talk){
+        $(this).addClass('btn-danger')
+      }
+      else{
+        $(this).removeClass('btn-danger')
+      }
       gapi.hangout.av.setMicrophoneMute(push_to_talk);
+    });
+    $("body").keydown(function(event) {
+      console.log(event.which);
+      if ( event.which == 77 ) {
+        gapi.hangout.av.setMicrophoneMute(!push_to_talk);
+      }
+    });
+    $("body").keyup(function(event) {
+      console.log(event.which);
+      if ( event.which == 77 ) {
+        gapi.hangout.av.setMicrophoneMute(push_to_talk);
+      }
     });
   }
 };
@@ -40,6 +72,7 @@ function init() {
   var apiReady = function(eventObj) {
     if (eventObj.isApiReady) {
       var mute = new Mute();
+      console.log(mute);
     }
   };
   gapi.hangout.onApiReady.add(apiReady);
