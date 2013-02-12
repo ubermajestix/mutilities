8bfabbb6c0de2dd5241635438e1f384186baf38e
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2013-02-11 22:38:52 -0800

    Cleanup last week's refactor
    Hangout sandbox was broken over the weekend, but its fixed now.

--- public/app.js
+++ public/app.js
@@ -1,9 +1,9 @@
 Hangout = {
-  me: function(){gapi.hangout.getLocalParticipant();},
+  me: function(){ return gapi.hangout.getLocalParticipant();},
   participants: function(){ return gapi.hangout.getParticipants();},
   mute: function(participant){
     if(_.isEmpty(arguments)){
-      participant = Hangout.me;
+      participant = Hangout.me();
     }
     gapi.hangout.av.muteParticipantMicrophone(participant.id);
   },
@@ -14,7 +14,6 @@ Hangout = {
 
 Mute = {
   init: function(){
-    console.log('init');
     Mute.push_to_talk_state = false;
     Mute.bind_mute_everyone();
     Mute.bind_push_to_talk();
@@ -26,62 +25,74 @@ Mute = {
 
   bind_mute_everyone: function(){
     $('button.mute').click(function(){ 
-      self.mute_everyone();
+      Mute.mute_everyone();
     });
   },
 
   mute_everyone: function(){
-    _.each(Mute.participants(), function(participant){
+    _.each(Hangout.participants(), function(participant){
       if(participant != Mute.me){
-        Mute.mute(participant);
+        Hangout.mute(participant);
       }
     });
   },
 
   bind_push_to_talk: function(){
-    console.log('bind_push_to_talk');
     Mute.bind_settings_dropdown();
-    $('button.push_to_talk').mousedown(function(){
+    $('button.push_to_talk').bind('mouseup mousedown', function(){
       Mute.push_to_talk();
       $(this).toggleClass('btn-danger');
     });
-    $('button.push_to_talk').mouseup(function(){
-      Mute.push_to_mute();
-      $(this).toggleClass('btn-danger');
-    });
+
     $("body").keydown(function(event) {
-      if ( event.which == 77 ) {
-        Mute.push_to_talk();
-        $(this).toggleClass('btn-danger');
+      if ( event.which == 77 && !Mute.pressed) {
+          Mute.pressed = true;
+          Mute.push_to_talk();
+          $('button.push_to_talk').toggleClass('btn-danger');
       }
     });
     $("body").keyup(function(event) {
-      if ( event.which == 77 ) {
-        Mute.push_to_mute();
-        $(this).toggleClass('btn-danger');
+      if ( event.which == 77 && Mute.pressed) {
+        Mute.pressed = false;
+        Mute.push_to_talk();
+        $('button.push_to_talk').toggleClass('btn-danger');
       }
     });
   },
 
   push_to_talk: function(){
-    Mute.push_to_talk_state ? Hangout.talk() : Hangout.mute();
+    if(Mute.push_to_talk_state){
+      Hangout.talk();
+    }
+    else{
+      Hangout.mute();
+    }
+    Mute.push_to_talk_state = !Mute.push_to_talk_state;
   },
 
   push_to_mute: function(){
-    Mute.push_to_talk_state ? Hangout.mute() : Hangout.talk();
+    if(Mute.push_to_talk_state){
+      Hangout.mute();
+    }
+    else{
+      Hangout.talk();
+    }
+    Mute.push_to_talk_state = !Mute.push_to_talk_state;
   },
 
   bind_settings_dropdown: function(){
     $('a.push_to_mute').hide();
     $('a.push_to_talk').click(function(){
       Mute.push_to_talk_state = true;
+      Hangout.mute();
       $('a.push_to_talk, a.push_to_mute').toggle();
-      $('button.push_to_talk').html(self.icon('bullhorn') + 'push to talk').addClass('btn-danger');
+      $('button.push_to_talk').html(Mute.icon('bullhorn') + 'push to talk').addClass('btn-danger');
     });
     $('a.push_to_mute').click(function(){
       Mute.push_to_talk_state = false;
+      Hangout.talk();
       $('a.push_to_talk, a.push_to_mute').toggle();
-      $('button.push_to_talk').html(self.icon('ban-circle') + 'push to mute');
+      $('button.push_to_talk').html(Mute.icon('ban-circle') + 'push to mute').removeClass('btn-danger');
     });
   }
 };
@@ -89,7 +100,6 @@ Mute = {
 function init() {
   var apiReady = function(eventObj) {
     if (eventObj.isApiReady) {
-      console.log('ready');
       Mute.init();
     }
   };
